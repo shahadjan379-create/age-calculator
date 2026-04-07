c75674674d7bae0bdb2631a6c8afec9c335bfceb
shahadjan379 <shahadjan379@gmail.com>
2026-04-06 17:43:00 -0700

    commit 3

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+n= int(input("enter a positive integer: "))
+i=0
+count=0
+sum=0
+
+while count<n:
+    sum += count
+    i +=1
+    count +=2
+
+print("the sum: ",sum)
+print("the square of the value: ",sum*sum)
+
+
